67aa7a108ba4b16614f6f72adb71e8b6ce8811cc
suraj480 <surajmaurya991@gmail.com>
2024-05-05 19:51:42 +0530

    basics-1

--- App.js
+++ App.js
@@ -1,73 +1,32 @@
-import React from 'react';
-import {Text, View, ScrollView, StatusBar, StyleSheet} from 'react-native';
-
-const Section = ({children, title}) => {
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>{title}</Text>
-      <Text style={styles.sectionDescription}>{children}</Text>
-    </View>
-  );
-};
+import React, {useState} from 'react';
+// eslint-disable-next-line prettier/prettier
+import {View, Text, Button} from 'react-native';
 
 const App = () => {
-  const isDarkMode = false; // Determine your dark mode logic here
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? '#000' : '#FFF',
+  let data = 100;
+  const [number, setNumber] = useState(200);
+  const buttonOne = () => {
+    let sum = data + 10;
+    setNumber(number + 10);
+    console.warn('Function called' + ' ' + sum + ' ' + number);
   };
-
+  const buttonTwo = a => {
+    let subtract = data - 5;
+    setNumber(number - 5);
+    console.warn(a + ' ' + subtract + ' ' + number);
+  };
   return (
-    // eslint-disable-next-line react-native/no-inline-styles
-    <View style={{flex: 1}}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <View
-          // eslint-disable-next-line react-native/no-inline-styles
-          style={{
-            backgroundColor: isDarkMode ? '#000' : '#FFF',
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            Reload your app to see changes!
-          </Section>
-          <Section title="Debug">
-            Debug your app by following instructions.
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next.
-          </Section>
-        </View>
-      </ScrollView>
+    <View>
+      <Text style={{fontSize: 30}}>Hello React Native</Text>
+      <View style={{marginHorizontal: 100, marginTop: 400}}>
+        <Button title="Press me" onPress={buttonOne} color="#FFA500"></Button>
+        <Button
+          title="Press me two"
+          onPress={() => buttonTwo('data from button two')}
+          color="green"></Button>
+      </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
